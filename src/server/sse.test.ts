8491dd9448d4e74ad793b84c1d02e725404f7089
Andrei Papancea <andrei@nlx.ai>
2025-04-07 22:16:47 -0400

    ! linting

--- src/server/sse.test.ts
+++ src/server/sse.test.ts
@@ -1,7 +1,8 @@
 import { SSEServerTransport } from "./sse.js";
 
 test("should initialize with provided sessionId", async () => {
-  const res: any = null;
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  const res: any = null; // mocking HTTP res as it's irrelevant to the test
   const server = new SSEServerTransport("/sse", res, "test-sessionId-123");
   expect(server.sessionId).toBe("test-sessionId-123");
 });
