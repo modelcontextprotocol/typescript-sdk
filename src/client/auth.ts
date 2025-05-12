4f55db95c24c974a7cd4e0e3f336faa557cf9bf2
Frédéric Barthelet <frederic.barthelet@gmail.com>
2025-05-12 15:21:03 +0200

    Save existing refresh_token in store if no new refresh_token is returned
    As described in https://datatracker.ietf.org/doc/html/draft-ietf-oauth-v2-1-12#name-refresh-token-response,
    server MAY return a new refresh_token. In case it doesn't, current implementation discard initial refresh
    token if it was still valid.

--- src/client/auth.ts
+++ src/client/auth.ts
@@ -378,7 +378,7 @@ export async function refreshAuthorization(
     throw new Error(`Token refresh failed: HTTP ${response.status}`);
   }
 
-  return OAuthTokensSchema.parse(await response.json());
+  return OAuthTokensSchema.parse({ refresh_token: refreshToken, ...(await response.json()) });
 }
 
 /**
