11563091cd0bf739b62350b80fc07138e7640199
Konstantin Konstantinov <konstantin@mach5technology.com>
2026-01-16 23:57:47 +0200

    feat: OSS SAST set up

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,3 +1,7 @@
 import { defineWorkspace } from 'vitest/config';
 
-export default defineWorkspace(['packages/**/vitest.config.js']);
+export default defineWorkspace([
+    'packages/**/vitest.config.js',
+    'examples/**/vitest.config.js',
+    'test/**/vitest.config.js',
+]);
